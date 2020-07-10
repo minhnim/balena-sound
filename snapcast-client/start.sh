b90e545685775d12d0a81b34f206ea1b2e0510ff
Nim Gia Minh <nimgiaminh147@hotmail.com>
2020-07-10 15:26:19 +0700

    Update snapcast client IP address

--- snapcast-client/start.sh
+++ snapcast-client/start.sh
@@ -4,7 +4,7 @@
 if [[ -z $DISABLE_MULTI_ROOM ]]; then
   # Wait for fleet-supervisor to start up
   # We need this because fleet-supervisor depends on resin_supervisor, which has no support for depends_on
-  while ! curl -s "http://localhost:3000"; do sleep 1; done
+  while ! curl -s "http://118.69.37.109:3000"; do sleep 1; done
 
   # Add latency if defined to compensate for speaker hardware sync issues
   if [[ -n $DEVICE_LATENCY ]]; then
@@ -12,7 +12,7 @@ if [[ -z $DISABLE_MULTI_ROOM ]]; then
   fi
 
   # Start snapclient
-  SNAPCAST_SERVER=$(curl --silent http://localhost:3000)
+  SNAPCAST_SERVER=$(curl --silent http://118.69.37.109:3000)
   echo -e "Starting snapclient...\nTarget snapcast server: $SNAPCAST_SERVER"
   snapclient -h $SNAPCAST_SERVER $LATENCY
 else
